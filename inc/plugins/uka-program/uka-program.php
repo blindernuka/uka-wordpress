2583a39af1518b44116d5cd80d0533fc690ddd8b
Vegard Andersen <ferska.vara@gmail.com>
2016-08-10 23:56:31 +0200

    curl fix?

--- inc/plugins/uka-program/uka-program.php
+++ inc/plugins/uka-program/uka-program.php
@@ -56,7 +56,7 @@ function get_eventgroup_data($eventgroup){
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_FAILONERROR, true);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 2000L);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 2);
 	$jsonData = curl_exec($ch);
 	curl_close($ch);
 	
