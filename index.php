4f9b43e3286864588799e8b73285984c698daca8
Vegard Andersen <ferska.vara@gmail.com>
2017-01-03 07:41:27 +0100

    index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <?php get_header(); ?>
 
 
-<main id="main">
+
 <section id="home" class="panel">
 <?php if (is_active_sidebar('home-top-left')): ?>
 	<aside id="home-top-left" class="widget-area">
@@ -31,52 +31,26 @@
 		<?php dynamic_sidebar('home-bottom-right'); ?>
 	</aside>
 <?php endif; ?>
-
 </section>
+
 <?php
-	$pages = intval(get_theme_mod('pages'));
-	if ($pages > 0){
-		for ($i = 1; $i < $pages + 1; $i++){
+	$panels = intval(get_theme_mod('panels'));
+	if ($panels > 0){
+		for ($i = 1; $i <= $panels; $i++){
 			$id = str_replace(' ', '-', strtolower(get_theme_mod('panel-'.$i.'-title')));
-			echo '<section class="panel panel-'.$i.'" id="'.$id.'" style="background-color:'.get_theme_mod('panel-'.$i.'-background-color').';">';
+			echo '<section class="panel panel-'.$i.'" id="'.$id.'">';
+			if (get_theme_mod('panel-'.$i.'-show-title')){
+				echo '<h1 class="widget-title">'.get_theme_mod('panel-'.$i.'-title').'</h1>';
+			}
 			dynamic_sidebar('panel-'.$i); 
 			echo '</section>';
 		}
 	}
+	
 ?>
-
-<section id="news" class="panel">
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			// End the loop.
-			endwhile;
-
-
-
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-</section>
-</main>
 	
 <?php get_footer(); ?>
+
+
+
+
