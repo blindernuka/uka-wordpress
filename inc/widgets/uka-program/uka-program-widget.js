84b9ec7a7c3e3192f240f9b221ac32bbb061c7c9
Vegard Andersen <ferska.vara@gmail.com>
2017-01-13 23:21:26 +0100

    Program widget scroll

--- inc/widgets/uka-program/uka-program-widget.js
+++ inc/widgets/uka-program/uka-program-widget.js
@@ -4,11 +4,10 @@ $(document).ready(function(){
 	$(".widget_uka_program").each(function(i, widget){
 		var days = $(widget).find(".uka-program");
 		var pastDays = $(widget).find(".past").length;
-		var lastPast = days[pastDays-1];
 		if (days.length == pastDays){
 			$(widget).scrollLeft(widget.scrollWidth - widget.offsetWidth);
 		}
-		else{
+		else if (pastDays > 0){
 			$(widget).scrollLeft(days[pastDays-1].offsetLeft+days[pastDays-1].offsetWidth);
 		}
 	});
