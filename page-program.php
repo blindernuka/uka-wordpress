c67bd7cbbef66625ca25df0dfeec212d528473cd
Vegard Andersen <ferska.vara@gmail.com>
2017-01-21 22:41:49 +0100

    page-program

--- page-program.php
+++ page-program.php
@@ -30,52 +30,64 @@ Template Name: Program
 			//echo __('Error retrieving eventgroup '.$data['id'], 'uka');
 		}
 		else if (count($program['events'] > 0)){
-			echo '<table class="uka-program">';
+			echo '<table class="uka-program '.$class.'">';
 			foreach ($program['events'] as $key => $event){
-				
+
 				if (($event['time_start']) && ($event['time_start'] > $day + 86400)){
-					if ($day){
-						echo '</tbody>';
-					}
-					//echo '<br>';
+										
 					$day = strtotime('midnight', $event['time_start']) + 21600;
-					//echo '<thead>';
-					echo '<thead class="uka-program-row uka-program-header uka-program-daytheme">';
-					echo '<tr>';
-					echo '<th colspan="100%">';
+					
+					if ($day < $today){
+						$class = "past";
+					}
+					else if ($day == $today){
+						$class = "today";
+					}
+					else{
+						$class = "future";
+					}
+					
+					//echo '<table class="uka-program '.$class.'">';
+					echo '<thead class="uka-program-header uka-program-daytheme '.$class.'">';
+					echo '<tr class="uka-program-row">';
+					echo '<th colspan="3">';
 					echo '<span class="uka-program-daytheme-title">';
 					foreach ($program['daythemes'] as $key => $daytheme){
-						if (strtotime('midnight', $event['time_start']) == $daytheme['date']){
+						if (strtotime('midnight', $day) == $daytheme['date']){
 							echo $daytheme['title'];
 							break;
 						}
 					}
 					echo '</span>';
-					
-					echo '<span class="uka-program-daytheme-date">';
-						echo '<span class="uka-program-daytheme-day">'.strftime('%A', $day).'</span>'.strftime(' %e. %B', $day).'</span>';
+					echo '<span class="uka-program-daytheme-date">'.strftime('%A %e. %B', $day).'</span>';
 					
 					echo '</th>';
 					echo '</tr>';
 					echo '</thead>';
-					echo '<tbody class="uka-day">';
+					echo '<tbody class="uka-day '.$class.'">';
 					
 				}
 				if (($event['time_start'] > $day) && ($event['time_start'] < $day + 86400)){
 					$title = $event['title'];
+					$location = $event['location'];
 					if ($event['link'] !== NULL){
 						$title = '<a href="'.$event['link'].'">'.$title.'</a>';
 					}
+					$ticket = $event['web_selling_status'];
+					if ($ticket == "sale" || $ticket == "old"){
+						$title = $title.'<a class="event-ticket" href="https://billett.blindernuka.no/billett/event/'.$event['id'].'" target="_blank"><i class="fa fa-ticket" aria-hidden="true"></i></a>';
+					}
 					echo '<tr class="uka-program-row uka-program-event ">';
-					echo '<td class="event-time">'.strftime('%H:%M', $event['time_start']).'</td>';
-					echo '<td class="event-location"><span>'.$event['location'];
-					echo '</span></td>';
 					echo '<td class="event-title">'.$title.'</td>';
+					echo '<td class="event-time">'.strftime('%H:%M', $event['time_start']).'</td>';
+					echo '<td class="event-location">'.$location.'</td>';
 					echo '</tr>';
 				}
 			}
+			
 			echo '</tbody>';
 			echo '</table>';
+			//echo '<div id="uka-program-spacer"></div>';
 		}
 		else{
 			//echo __('No events in eventgroup '.$data['id'], 'uka');
